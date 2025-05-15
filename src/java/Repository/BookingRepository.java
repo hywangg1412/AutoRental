7b93be0ccb6a8242ad35fcda2243865c49ca2d2c
huydev <hyperrhy1412@gmail.com>
2025-05-15 21:18:29 +0700

    Format some file

--- src/java/Repository/BookingRepository.java
+++ src/java/Repository/BookingRepository.java
@@ -32,9 +32,4 @@ public class BookingRepository implements IBookingRepository{
     public List<Booking> findAll() throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
-    
-
-    
-
 }
