0df02649c7347a1959bb8ca8d35609a4f68784eb
huydev <hyperrhy1412@gmail.com>
2025-05-23 00:26:00 +0700

    signIn/signUp DTO for user

---
+++ src/java/Model/dto/UserRegisterDTO.java
@@ -0,0 +1,97 @@
+package Model.dto;
+
+import java.time.LocalDate;
+
+public class UserRegisterDTO {
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+    private String gender;
+
+    public UserRegisterDTO() {
+    }
+
+    public UserRegisterDTO(String username, String password, String email, String phoneNumber,
+            String firstName, String lastName, LocalDate dob, String gender) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+} 
