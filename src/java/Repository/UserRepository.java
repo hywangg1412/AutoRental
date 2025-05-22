a924cdc62f5a4a1d347e097469e670d29f3bbe81
huydev <hyperrhy1412@gmail.com>
2025-05-23 00:27:03 +0700

    testing User repo

--- src/java/Repository/UserRepository.java
+++ src/java/Repository/UserRepository.java
@@ -1,11 +1,19 @@
 package Repository;
 
+import Config.DBContext;
 import Model.User;
 import Repository.Interfaces.IUserRepository;
-import java.sql.SQLException;
 import java.util.List;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class UserRepository implements IUserRepository{
+public class UserRepository implements IUserRepository {
+
+    private DBContext dbContext = new DBContext();
+
+    public UserRepository() {
+    }
 
     @Override
     public void add(User entity) throws SQLException {
@@ -31,5 +39,31 @@ public class UserRepository implements IUserRepository{
     public List<User> findAll() throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
+    public User findByUsernameAndPassword(String username, String password) {
+        String sql = "SELECT * FROM Users WHERE Username = ? AND Password = ?";
+        try (Connection conn = dbContext.getConnection(); PreparedStatement st = conn.prepareStatement(sql)) {
+
+            st.setString(1, username);
+            st.setString(2, password);
+
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    return mapResultSetToUser(rs);
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error While Finding Username and Password - " + ex.getMessage());
+        }
+        return null;
+    }
+
+    private User mapResultSetToUser(ResultSet rs) throws SQLException {
+        User u = new User();
+//        u.setUserId(rs.getInt("UserId"));
+//        u.setUsername(rs.getString("Username"));
+//        u.setPassword(rs.getString("Password"));
+        return u;
+    }
+
 }
