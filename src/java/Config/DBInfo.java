78de8b503e749fca917cdf221ed0052226870f1d
huydev <hyperrhy1412@gmail.com>
2025-05-27 16:48:56 +0700

    change db config

--- src/java/Config/DBInfo.java
+++ src/java/Config/DBInfo.java
@@ -1,8 +1,8 @@
 package Config;
 
 public interface DBInfo {
-    String DRIVER_NAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    String URL = "jdbc:sqlserver://localhost:1433;databaseName=AutoRental;encrypt=true;trustServerCertificate=true";
-    String USERNAME = "sa";
-    String PASSWORD = "123";
+    public static final String DRIVER_NAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=AutoRental;encrypt=true;trustServerCertificate=true";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "123";
 }
