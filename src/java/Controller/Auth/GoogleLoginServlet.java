7c6155e47a768297e497545a275fe1b90b3e4cab
huydev <hyperrhy1412@gmail.com>
2025-05-29 22:23:35 +0700

    Update Google OAuth login: handle callback, save user session, and redirect after login

--- src/java/Controller/Auth/GoogleLoginServlet.java
+++ src/java/Controller/Auth/GoogleLoginServlet.java
@@ -1,6 +1,6 @@
 package Controller.Auth;
 
-import Service.auth.GoogleOAuthService;
+import Service.auth.GoogleAuthService;
 import Model.Entity.OAuth.GoogleUser;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,18 +8,43 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
+// google/login
 public class GoogleLoginServlet extends HttpServlet {
+
+    private GoogleAuthService googleAuthService;
+
+    @Override
+    public void init() {
+        googleAuthService = new GoogleAuthService();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String code = request.getParameter("code");
-        GoogleOAuthService gg = new GoogleOAuthService();
-        String accessToken = gg.getToken(code);
-        System.out.println(accessToken);
-        GoogleUser usser = gg.getUserInfo(accessToken);
-        System.out.println(usser);
+
+        if (code == null) {
+            String authUrl = googleAuthService.getAuthorizationUrl();
+            response.sendRedirect(authUrl);
+        } else {
+            try {
+                GoogleUser googleUser = googleAuthService.getUserInfo(code);
+                
+                // Test if login success or not
+                System.out.println("User login: " + googleUser.toString());
+                // Session control here
+                HttpSession session = request.getSession();
+                session.setAttribute("googleUser", googleUser);
+
+                response.sendRedirect(request.getContextPath() + "/pages/index.jsp");
+            } catch (Exception e) {
+                request.setAttribute("errorMsg", "Google login failed - " + e.getMessage());
+                request.getRequestDispatcher("pages/Error.jsp").forward(request, response);
+            }
+        }
+
     }
 
     @Override
