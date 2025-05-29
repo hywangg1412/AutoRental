75ed88f84ec1fa5f84fa1e842ee2b163dfa47b8a
huydev <hyperrhy1412@gmail.com>
2025-05-29 22:22:19 +0700

    update GoogleOAuthor properties

--- src/java/Constant/OAuthConstants.java
+++ src/java/Constant/OAuthConstants.java
@@ -1,13 +1,19 @@
 package Constant;
 
 public class OAuthConstants {
-    // Google OAuth
-    public static final String GOOGLE_CLIENT_ID = System.getenv("GOOGLE_CLIENT_ID");
-    public static final String GOOGLE_CLIENT_SECRET = System.getenv("GOOGLE_CLIENT_SECRET");
-    public static final String GOOGLE_REDIRECT_URI = System.getenv("GOOGLE_REDIRECT_URI");
-    public static final String GOOGLE_GRANT_TYPE = System.getenv("GOOGLE_GRANT_TYPE");
-    public static final String GOOGLE_LINK_GET_TOKEN = System.getenv("GOOGLE_LINK_GET_TOKEN");
-    public static final String GOOGLE_LINK_GET_USER_INFO = System.getenv("GOOGLE_LINK_GET_USER_INFO");
+// Google OAuth
+
+
+    // Google OAuth Endpoints
+    public static final String GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/auth";
+    public static final String GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
+    public static final String GOOGLE_USER_INFO_URL = "https://www.googleapis.com/oauth2/v1/userinfo";
+
+    public static final String GOOGLE_CLIENT_ID = "669490144407-kiueqs7v7sv1drt65s709fgs3l29rts6.apps.googleusercontent.com";
+    public static final String GOOGLE_CLIENT_SECRET = "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";
+    public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/autorental/googleLogin";
+    public static final String GOOGLE_EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+    public static final String GOOGLE_PROFILE_SCOPE = "https://www.googleapis.com/auth/userinfo.profile";
 
     // Facebook OAuth
     public static final String FACEBOOK_CLIENT_ID = System.getenv("FACEBOOK_CLIENT_ID");
