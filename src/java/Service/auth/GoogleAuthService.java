c17f4b6bbeb02b8f9e04bb081b4efef9fbde1ea4
huydev <hyperrhy1412@gmail.com>
2025-05-30 19:03:51 +0700

    Update: fix return value

--- src/java/Service/auth/GoogleAuthService.java
+++ src/java/Service/auth/GoogleAuthService.java
@@ -14,53 +14,54 @@ import com.google.api.services.oauth2.model.Userinfo;
 import java.io.IOException;
 import java.util.Arrays;
 
-
-public class GoogleAuthService extends OAuthConstants{
+public class GoogleAuthService extends OAuthConstants {
 
     private GoogleAuthorizationCodeFlow flow;
-    
-    public GoogleAuthService(){
+
+    public GoogleAuthService() {
         try {
             flow = new GoogleAuthorizationCodeFlow.Builder(
-            new NetHttpTransport(),
-            GsonFactory.getDefaultInstance(),
+                    new NetHttpTransport(),
+                    GsonFactory.getDefaultInstance(),
                     GOOGLE_CLIENT_ID,
-                    GOOGLE_CLIENT_SECRET, 
+                    GOOGLE_CLIENT_SECRET,
                     Arrays.asList(OAuthConstants.GOOGLE_EMAIL_SCOPE, OAuthConstants.GOOGLE_PROFILE_SCOPE))
                     .setAccessType("offline")
                     .build();
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Failed to initialize Google Auth Service - " + e.toString());
         }
     }
-    
+
     public String getAuthorizationUrl() {
         return flow.newAuthorizationUrl()
-            .setRedirectUri(GOOGLE_REDIRECT_URI)
-            .build();
+                .setRedirectUri(GOOGLE_REDIRECT_URI)
+                .build();
     }
-    
+
     public GoogleUser getUserInfo(String code) throws IOException {
         TokenResponse tokenResponse = flow.newTokenRequest(code)
-            .setRedirectUri(OAuthConstants.GOOGLE_REDIRECT_URI)
-            .execute();
-            
+                .setRedirectUri(OAuthConstants.GOOGLE_REDIRECT_URI)
+                .execute();
+
         GoogleCredential credential = new GoogleCredential()
-            .setAccessToken(tokenResponse.getAccessToken());
-            
+                .setAccessToken(tokenResponse.getAccessToken());
+
         Oauth2 oauth2 = new Oauth2.Builder(
-            new NetHttpTransport(),
-            GsonFactory.getDefaultInstance(),
-            credential)
-            .build();
-            
+                new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                credential)
+                .build();
+
         Userinfo userInfo = oauth2.userinfo().get().execute();
-        
+
         return new GoogleUser(
-            userInfo.getId(),
-            userInfo.getEmail(),
-            userInfo.getName(),
-            userInfo.getPicture()
+                userInfo.getId(),
+                userInfo.getEmail(),
+                userInfo.getFamilyName(),
+                userInfo.getGivenName(),
+                userInfo.getPicture(),
+                userInfo.getVerifiedEmail()
         );
     }
 }
