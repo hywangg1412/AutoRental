c7d085e5298d9fd931af84077f4d04d290edbf91
huydev <hyperrhy1412@gmail.com>
2025-05-31 00:41:23 +0700

    Add : methods for user authentication and lookup

--- src/java/Repository/Interfaces/IUserRepository.java
+++ src/java/Repository/Interfaces/IUserRepository.java
@@ -6,4 +6,6 @@ import Model.Entity.User;
 
 public interface IUserRepository extends Repository<User, Integer>{
     User findByUsernameAndPassword(String username, String password);
+    
+    User findByEmail(String email);
 }
