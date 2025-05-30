3a699a28fb7d7bc1825a98694991379384298ac3
huydev <hyperrhy1412@gmail.com>
2025-05-31 00:38:09 +0700

    Implement: Google OAuth register flow and user creation logic

---
+++ src/java/Controller/Auth/GoogleRegisterServlet.java
@@ -0,0 +1,73 @@
+package Controller.Auth;
+
+import Mapper.UserMapper;
+import Model.Entity.OAuth.GoogleUser;
+import Model.Entity.User;
+import Service.UserService;
+import Service.auth.GoogleAuthService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// googleRegister
+public class GoogleRegisterServlet extends HttpServlet {
+
+    private GoogleAuthService googleAuthService;
+    private UserMapper userMapper;
+    private UserService userService;
+
+    @Override
+    public void init() {
+        googleAuthService = new GoogleAuthService();
+        userMapper = new UserMapper();
+        userService = new UserService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String code = request.getParameter("code");
+
+        if (code == null) {
+            String authUrl = googleAuthService.getAuthorizationRegisterUrl();
+            response.sendRedirect(authUrl);
+        } else {
+
+            try {
+                GoogleUser googleUser = googleAuthService.getUserInfoRegister(code);
+                User user = userService.findByEmail(googleUser.getEmail());
+
+                if (user != null) {
+                    request.setAttribute("errMsg", "Email with this account already exist");
+                    request.getRequestDispatcher(request.getContextPath() + "/pages/authen/SignUp.jsp").forward(request, response);
+                    return;
+                }
+
+                user = userMapper.mapGoogleUserToUser(googleUser);
+                try {
+                    userService.add(user);
+                    request.getSession().setAttribute("user", user);
+                    response.sendRedirect(request.getContextPath() + "/pages/index.jsp");
+                } catch (Exception ex) {
+                    System.out.println("Can't add user to the system");
+                    request.setAttribute("errorMsg", "Can't add user to the system - " + ex.getMessage());
+                    request.getRequestDispatcher("pages/Error.jsp").forward(request, response);
+                }
+
+            } catch (Exception e) {
+                System.out.println("Google register failed");
+                request.setAttribute("errorMsg", "Google register failed - " + e.getMessage());
+                request.getRequestDispatcher("pages/Error.jsp").forward(request, response);
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+}
