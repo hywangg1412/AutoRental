fc8e839d73bab9cf3056c3f382abe826a21988fd
huydev <hyperrhy1412@gmail.com>
2025-05-31 20:21:26 +0700

    Implement: Facebook register OAuth authentication

--- src/java/Service/auth/FacebookAuthService.java
+++ src/java/Service/auth/FacebookAuthService.java
@@ -11,14 +11,15 @@ import java.net.URLEncoder;
 import org.json.JSONObject;
 
 public class FacebookAuthService extends OAuthConstants {
+
     public String getAuthorizationUrl() {
         try {
-            String url = FACEBOOK_AUTH_URL +
-                "?client_id=" + URLEncoder.encode(FACEBOOK_CLIENT_ID, "UTF-8") +
-                "&redirect_uri=" + URLEncoder.encode(FACEBOOK_REDIRECT_URI, "UTF-8") +
-                "&scope=" + URLEncoder.encode(FACEBOOK_SCOPE, "UTF-8") +
-                "&response_type=code" +
-                "&state=" + URLEncoder.encode(FACEBOOK_STATE, "UTF-8");
+            String url = FACEBOOK_AUTH_URL
+                    + "?client_id=" + URLEncoder.encode(FACEBOOK_CLIENT_ID, "UTF-8")
+                    + "&redirect_uri=" + URLEncoder.encode(FACEBOOK_REDIRECT_URI, "UTF-8")
+                    + "&scope=" + URLEncoder.encode(FACEBOOK_SCOPE, "UTF-8")
+                    + "&response_type=code"
+                    + "&state=" + URLEncoder.encode(FACEBOOK_STATE, "UTF-8");
             return url;
         } catch (Exception e) {
             throw new RuntimeException("Failed to build Facebook authorization URL", e);
@@ -26,18 +27,18 @@ public class FacebookAuthService extends OAuthConstants {
     }
 
     public FacebookUser getUserInfo(String code) throws IOException {
-        String tokenUrl = FACEBOOK_TOKEN_URL +
-            "?client_id=" + URLEncoder.encode(FACEBOOK_CLIENT_ID, "UTF-8") +
-            "&redirect_uri=" + URLEncoder.encode(FACEBOOK_REDIRECT_URI, "UTF-8") +
-            "&client_secret=" + URLEncoder.encode(FACEBOOK_CLIENT_SECRET, "UTF-8") +
-            "&code=" + URLEncoder.encode(code, "UTF-8") +
-            "&grant_type=" + URLEncoder.encode(FACEBOOK_GRANT_TYPE, "UTF-8");
+        String tokenUrl = FACEBOOK_TOKEN_URL
+                + "?client_id=" + URLEncoder.encode(FACEBOOK_CLIENT_ID, "UTF-8")
+                + "&redirect_uri=" + URLEncoder.encode(FACEBOOK_REDIRECT_URI, "UTF-8")
+                + "&client_secret=" + URLEncoder.encode(FACEBOOK_CLIENT_SECRET, "UTF-8")
+                + "&code=" + URLEncoder.encode(code, "UTF-8")
+                + "&grant_type=" + URLEncoder.encode(FACEBOOK_GRANT_TYPE, "UTF-8");
 
         JSONObject tokenResponse = getJsonFromUrl(tokenUrl);
         String accessToken = tokenResponse.getString("access_token");
 
-        String userInfoUrl = FACEBOOK_USER_INFO_URL +
-            "&access_token=" + URLEncoder.encode(accessToken, "UTF-8");
+        String userInfoUrl = FACEBOOK_USER_INFO_URL
+                + "&access_token=" + URLEncoder.encode(accessToken, "UTF-8");
         JSONObject userInfo = getJsonFromUrl(userInfoUrl);
 
         String id = userInfo.optString("id", null);
@@ -71,4 +72,47 @@ public class FacebookAuthService extends OAuthConstants {
         in.close();
         return new JSONObject(response.toString());
     }
+
+    public String getAuthorizationRegisterUrl() {
+        try {
+            String url = FACEBOOK_AUTH_URL
+                    + "?client_id=" + URLEncoder.encode(FACEBOOK_CLIENT_ID, "UTF-8")
+                    + "&redirect_uri=" + URLEncoder.encode(FACEBOOK_REDIRECT_URI_REGISTER, "UTF-8")
+                    + "&scope=" + URLEncoder.encode(FACEBOOK_SCOPE, "UTF-8")
+                    + "&response_type=code"
+                    + "&state=" + URLEncoder.encode(FACEBOOK_STATE, "UTF-8");
+            return url;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to build Facebook authorization URL", e);
+        }
+    }
+
+    public FacebookUser getRegisterUserInfo(String code) throws IOException {
+        String tokenUrl = FACEBOOK_TOKEN_URL
+                + "?client_id=" + URLEncoder.encode(FACEBOOK_CLIENT_ID, "UTF-8")
+                + "&redirect_uri=" + URLEncoder.encode(FACEBOOK_REDIRECT_URI_REGISTER, "UTF-8")
+                + "&client_secret=" + URLEncoder.encode(FACEBOOK_CLIENT_SECRET, "UTF-8")
+                + "&code=" + URLEncoder.encode(code, "UTF-8")
+                + "&grant_type=" + URLEncoder.encode(FACEBOOK_GRANT_TYPE, "UTF-8");
+
+        JSONObject tokenResponse = getJsonFromUrl(tokenUrl);
+        String accessToken = tokenResponse.getString("access_token");
+
+        String userInfoUrl = FACEBOOK_USER_INFO_URL
+                + "&access_token=" + URLEncoder.encode(accessToken, "UTF-8");
+        JSONObject userInfo = getJsonFromUrl(userInfoUrl);
+
+        String id = userInfo.optString("id", null);
+        String email = userInfo.optString("email", null);
+        String name = userInfo.optString("name", null);
+        String avatarUrl = null;
+        if (userInfo.has("picture")) {
+            JSONObject picture = userInfo.getJSONObject("picture");
+            if (picture.has("data")) {
+                avatarUrl = picture.getJSONObject("data").optString("url", null);
+            }
+        }
+        return new FacebookUser(id, email, name, avatarUrl);
+    }
+
 }
