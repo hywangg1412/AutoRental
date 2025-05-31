81fda3650e8c0a7d455955f53e0cb2ace11a2ef1
huydev <hyperrhy1412@gmail.com>
2025-05-31 20:17:27 +0700

    Add: Handle Facebook OAuth registration

---
+++ src/java/Controller/Auth/FacebookRegisterServlet.java
@@ -0,0 +1,63 @@
+package Controller.Auth;
+
+import Mapper.UserMapper;
+import Model.Entity.OAuth.FacebookUser;
+import Model.Entity.User;
+import Service.UserService;
+import Service.auth.FacebookAuthService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+//facebookLogin
+public class FacebookRegisterServlet extends HttpServlet {
+
+    private FacebookAuthService facebookAuthService;
+    private UserMapper userMapper;
+    private UserService userService;
+
+    @Override
+    public void init() {
+        facebookAuthService = new FacebookAuthService();
+        userMapper = new UserMapper();
+        userService = new UserService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String code = request.getParameter("code");
+        if (code == null) {
+            String authUrl = facebookAuthService.getAuthorizationRegisterUrl();
+            response.sendRedirect(authUrl);
+        } else {
+            try {
+                FacebookUser facebookUser = facebookAuthService.getRegisterUserInfo(code);
+                User user = userService.findByEmail(facebookUser.getEmail());
+                if (user != null) {
+                    request.setAttribute("errMsg", "An account with this facebook already exists.");
+                    request.getRequestDispatcher("/pages/authen/SignUp.jsp").forward(request, response);
+                    return;
+                }
+                user = userMapper.mapFacebookUserToUser(facebookUser);
+                try {
+                    userService.add(user);
+                    request.getSession().setAttribute("user", user);
+                    response.sendRedirect(request.getContextPath() + "/pages/index.jsp");
+                } catch (Exception ex) {
+                    System.out.println("Can't add user to the system");
+                    request.setAttribute("errorMsg", "Can't add user to the system - " + ex.getMessage());
+                    request.getRequestDispatcher("pages/Error.jsp").forward(request, response);
+                }
+            } catch (Exception e) {
+                request.setAttribute("errorMsg", "Facebook login failed - " + e.getMessage());
+                request.getRequestDispatcher("pages/Error.jsp").forward(request, response);
+            }
+        }
+    }
+
+}
