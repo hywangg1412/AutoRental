d60eae342684be48d41e448ec39a1c003cebab0d
huydev <hyperrhy1412@gmail.com>
2025-06-11 00:48:25 +0700

    feat<>: add discount module

--- src/java/Model/Entity/Discount.java
+++ src/java/Model/Entity/Discount.java
@@ -1,60 +1,107 @@
 package Model.Entity;
 
-import java.util.Date;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class Discount {
-    private int discountId;
-    private Date startDate;
-    private Date endDate;
+    private UUID discountId;
+    private String discountName;
+    private String description;
+    private String discountType; // 'Percent' or 'Fixed'
+    private BigDecimal discountValue;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
     private boolean isActive;
-    private String type;
+    private LocalDateTime createdDate;
 
-    public Discount() {
-    }
+    public Discount() {}
 
-    public int getDiscountId() {
+    public UUID getDiscountId() {
         return discountId;
     }
 
-    public void setDiscountId(int discountId) {
+    public void setDiscountId(UUID discountId) {
         this.discountId = discountId;
     }
 
-    public Date getStartDate() {
+    public String getDiscountName() {
+        return discountName;
+    }
+
+    public void setDiscountName(String discountName) {
+        this.discountName = discountName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDiscountType() {
+        return discountType;
+    }
+
+    public void setDiscountType(String discountType) {
+        this.discountType = discountType;
+    }
+
+    public BigDecimal getDiscountValue() {
+        return discountValue;
+    }
+
+    public void setDiscountValue(BigDecimal discountValue) {
+        this.discountValue = discountValue;
+    }
+
+    public LocalDateTime getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(Date startDate) {
+    public void setStartDate(LocalDateTime startDate) {
         this.startDate = startDate;
     }
 
-    public Date getEndDate() {
+    public LocalDateTime getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(Date endDate) {
+    public void setEndDate(LocalDateTime endDate) {
         this.endDate = endDate;
     }
-
-    public boolean isIsActive() {
+    
+    public boolean isActive() {
         return isActive;
     }
 
-    public void setIsActive(boolean isActive) {
+    public void setActive(boolean isActive) {
         this.isActive = isActive;
     }
 
-    public String getType() {
-        return type;
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
     }
 
     @Override
     public String toString() {
-        return "Discount{" + "discountId=" + discountId + ", startDate=" + startDate + ", endDate=" + endDate + ", isActive=" + isActive + ", type=" + type + '}';
+        return "Discount{" +
+                "discountId=" + discountId +
+                ", discountName='" + discountName + '\'' +
+                ", description='" + description + '\'' +
+                ", discountType='" + discountType + '\'' +
+                ", discountValue=" + discountValue +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", isActive=" + isActive +
+                ", createdDate=" + createdDate +
+                '}';
     }
-    
 }
