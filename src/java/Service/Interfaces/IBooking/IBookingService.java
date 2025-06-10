56db7a1026611782319c0a68fe03dc8ac5f83038
huydev <hyperrhy1412@gmail.com>
2025-06-11 00:58:30 +0700

    feat<>: add Booking services and interfaces related module for booking data access

--- src/java/Service/Interfaces/IBooking/IBookingService.java
+++ src/java/Service/Interfaces/IBooking/IBookingService.java
@@ -1,6 +1,9 @@
-package Service.Interfaces;
-import Model.Entity.Booking;
+package Service.Interfaces.IBooking;
+import Model.Entity.Booking.Booking;
+import Service.Interfaces.Service;
+import java.sql.SQLException;
+import java.util.List;
 
 public interface IBookingService extends Service<Booking> {
-    
+    List<Booking> findByStatus(String status) throws SQLException;
 }
