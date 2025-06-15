5d9304b3e09bf1e871a34733604e131b9e86780a
huydev <hyperrhy1412@gmail.com>
2025-06-15 15:46:32 +0700

    refactor<>: fix car-list display servlet

--- src/java/Controller/Car/CarServlet.java
+++ src/java/Controller/Car/CarServlet.java
@@ -1,11 +1,10 @@
 package Controller.Car;
 
-
 import Model.Entity.Car.Car;
 import Service.Car.CarService;
-import Service.Interfaces.ICarService;
 import Exception.EmptyDataException;
-
+import Model.DTO.CarListItemDTO;
+import Service.Car.CarListService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,23 +17,20 @@ import java.util.List;
 @WebServlet("/pages/car")
 public class CarServlet extends HttpServlet {
 
-    private CarService carService;
+    private CarListService carListService;
 
     @Override
     public void init() throws ServletException {
-        carService = new CarService();
+        carListService = new CarListService();
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-            List<Car> carList = carService.getAllCars();
+            List<CarListItemDTO> carList = carListService.getAll(); 
             System.out.println("Số lượng xe lấy được: " + carList.size());
             request.setAttribute("carList", carList);
-            request.getRequestDispatcher("/pages/car.jsp").forward(request, response);
-        } catch (EmptyDataException e) {
-            request.setAttribute("error", "Không có xe nào khả dụng.");
             request.getRequestDispatcher("/pages/car.jsp").forward(request, response);
         } catch (Exception e) {
             e.printStackTrace();
