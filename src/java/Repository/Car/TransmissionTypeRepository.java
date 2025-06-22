dbfd4d4595cde0b67b8fd9620df3b74f41b95636
longg1708 <longabc932@gmail.com>
2025-06-23 00:25:45 +0700

    sua repo cua car

--- src/java/Repository/Car/TransmissionTypeRepository.java
+++ src/java/Repository/Car/TransmissionTypeRepository.java
@@ -9,10 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class TranmissionTypeRepository implements ITransmissionTypeRepository{
+public class TransmissionTypeRepository implements ITransmissionTypeRepository{
     private DBContext dbContext;
 
-    public TranmissionTypeRepository() {
+    public TransmissionTypeRepository() {
         dbContext = new DBContext();
     }
     
@@ -21,8 +21,8 @@ public class TranmissionTypeRepository implements ITransmissionTypeRepository{
         String sql = "INSERT INTO TransmissionType (TransmissionTypeId, TransmissionName) VALUES (?, ?)";
         try (var conn = dbContext.getConnection();
              var ps = conn.prepareStatement(sql)) {
-            ps.setObject(1, entity.getTranmissionTypeId());
-            ps.setString(2, entity.getTranmissionName());
+            ps.setObject(1, entity.getTransmissionTypeId());
+            ps.setString(2, entity.getTransmissionName());
             ps.executeUpdate();
             return entity;
         }
@@ -47,8 +47,8 @@ public class TranmissionTypeRepository implements ITransmissionTypeRepository{
         String sql = "UPDATE TransmissionType SET TransmissionName = ? WHERE TransmissionTypeId = ?";
         try (var conn = dbContext.getConnection();
              var ps = conn.prepareStatement(sql)) {
-            ps.setString(1, entity.getTranmissionName());
-            ps.setObject(2, entity.getTranmissionTypeId());
+            ps.setString(1, entity.getTransmissionName());
+            ps.setObject(2, entity.getTransmissionTypeId());
             return ps.executeUpdate() > 0;
         }
     }
@@ -64,23 +64,28 @@ public class TranmissionTypeRepository implements ITransmissionTypeRepository{
     }
 
     @Override
-    public List<TransmissionType> findAll() throws SQLException {
+    public List<TransmissionType> findAll() {
+        List<TransmissionType> list = new ArrayList<>();
         String sql = "SELECT * FROM TransmissionType";
-        List<TransmissionType> transmissionTypes = new ArrayList<>();
-        try (var conn = dbContext.getConnection();
+        try (var conn = new DBContext().getConnection();
              var ps = conn.prepareStatement(sql);
              var rs = ps.executeQuery()) {
             while (rs.next()) {
-                transmissionTypes.add(mapResultSetToTransmissionType(rs));
+                TransmissionType t = new TransmissionType();
+                t.setTransmissionTypeId(UUID.fromString(rs.getString("TransmissionTypeId")));
+                t.setTransmissionName(rs.getString("TransmissionName"));
+                list.add(t);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return transmissionTypes;
+        return list;
     }
 
     private TransmissionType mapResultSetToTransmissionType(ResultSet rs) throws SQLException {
         TransmissionType transmissionType = new TransmissionType();
-        transmissionType.setTranmissionTypeId(UUID.fromString(rs.getString("TransmissionTypeId")));
-        transmissionType.setTranmissionName(rs.getString("TransmissionName"));
+        transmissionType.setTransmissionTypeId(UUID.fromString(rs.getString("TransmissionTypeId")));
+        transmissionType.setTransmissionName(rs.getString("TransmissionName"));
         return transmissionType;
     }
 }
