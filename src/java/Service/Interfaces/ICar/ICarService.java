d8a4d87b0b27bfdc97399aac38fb71031f59cbd6
longg1708 <longabc932@gmail.com>
2025-06-23 00:26:59 +0700

    sua IcarService

--- src/java/Service/Interfaces/ICar/ICarService.java
+++ src/java/Service/Interfaces/ICar/ICarService.java
@@ -7,4 +7,16 @@ import java.util.List;
 
 public interface ICarService extends Service<Car>{
     List<Car> findAll() throws SQLException;
+    List<Car> findByPage(int offset, int limit) throws SQLException;
+    int countAll() throws SQLException;
+    List<Car> searchByKeyword(String keyword, int offset, int limit) throws SQLException;
+    int countByKeyword(String keyword) throws SQLException;
+    List<Car> filterCars(
+        String[] brandIds, String[] fuelTypeIds, String[] seats, String[] categoryIds,
+        String[] statuses, String[] featureIds, String[] transmissionTypeIds, String sort, String keyword, int offset, int limit
+    ) throws SQLException;
+    int countFilteredCars(
+        String[] brandIds, String[] fuelTypeIds, String[] seats, String[] categoryIds,
+        String[] statuses, String[] featureIds, String[] transmissionTypeIds, String keyword
+    ) throws SQLException;
 }
