c3c3889b918ca0874d9be909f343caa8adb76efa
hongquandt <160511165+hongquandt@users.noreply.github.com>
2025-07-11 11:50:22 +0700

    feat:admin manage (#72)
    * staff src
    
    * staff web
    
    * staff web

--- src/java/Model/Constants/BookingStatusConstants.java
+++ src/java/Model/Constants/BookingStatusConstants.java
@@ -3,12 +3,6 @@ package Model.Constants;
 public class BookingStatusConstants {
     public static final String PENDING = "Pending";
     public static final String CONFIRMED = "Confirmed";
-    public static final String AWAITING_PAYMENT = "AwaitingPayment";
-    public static final String DEPOSIT_PAID = "DepositPaid";
-    public static final String CONTRACT_SIGNED = "ContractSigned";
-    public static final String FULLY_PAID = "FullyPaid";
-    public static final String IN_PROGRESS = "InProgress";
-    public static final String COMPLETED = "Completed";
     public static final String CANCELLED = "Cancelled";
-    public static final String REJECTED = "Rejected";
+    public static final String COMPLETED = "Completed";
 }
