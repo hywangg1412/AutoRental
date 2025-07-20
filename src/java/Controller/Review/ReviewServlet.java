5ccd26637817096f3f804657b9efc16c39352091
datnt04 <162810687+datnt04@users.noreply.github.com>
2025-07-20 23:30:48 +0700

    feat: user feedback (#105)
    * feedback
    
    * notification
    
    * css notification
    
    * notification
    
    * feedback
    
    * css feedback
    
    * add session usernamme
    
    * feedback
    
    * feedback
    
    * feedback
    
    * feedback
    
    * feedback
    
    * feedback
    
    * feedback
    
    * feedback
    
    * feedback
    
    * feedback

---
+++ src/java/Controller/Review/ReviewServlet.java
@@ -0,0 +1,9 @@
+package Controller.Review;
+
+// TEMPORARILY DISABLED DUE TO COMPILATION ISSUES
+// Will be restored after fixing servlet dependencies
+ 
+public class ReviewServlet {
+    // Servlet code temporarily commented out to fix deployment
+    // TODO: Restore after fixing classpath issues
+} 
